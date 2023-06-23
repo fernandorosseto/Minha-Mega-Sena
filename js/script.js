4e9ac752073710b43c3f7f9d8b1bd8a1f8386402
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-06-23 14:33:24 -0300

    utilização do loop para criar a matriz contendo os valores de 1 a 60.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 //resultados Mega Sena anteriores = criar um banco de dados		
 //Matriz contendo os números de 1 a 60, semelhante ao sorteiro original
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60];
+let numeros = [];
+let atual = 1;
+while (atual <= 60){
+    numeros.push(atual)
+    atual++;
+    console.log(numeros)
+}
 
 /* =============================================================== */
 
