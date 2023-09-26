cf7982b89e787f625d9613a5ac7a7bfde5401e50
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2023-09-26 17:22:16 -0300

    v.2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 //resultados Mega Sena anteriores = criar um banco de dados
-//Matriz contendo os números de 1 a 60, semelhante ao sorteiro original
-let numeros = [];
-let atual = 1;
-while (atual <= 60) {
-  numeros.push(atual);
-  atual++;
-}
 
 /* =============================================================== */
 
@@ -13,47 +6,51 @@ while (atual <= 60) {
     2 - Retirando o número sorteado
     3 - Verificando a matriz no console 
 */
-let numerosEscolhidos = [];
-for (let i = 0; i < 6; i++) {
-  let n1 = numeros[Math.floor(Math.random() * numeros.length)];
-  numeros.splice(numeros.indexOf(n1), 1);
-  numerosEscolhidos.push(n1);
+
+function randomNumbers() {
+	//Matriz contendo os números de 1 a 60, semelhante ao sorteiro original
+	let numeros = [];
+	let atual = 1;
+	while (atual <= 60) {
+		numeros.push(atual);
+		atual++;
+	}
+
+	let numerosEscolhidos = [];
+	for (let i = 0; i < 6; i++) {
+		let n1 = numeros[Math.floor(Math.random() * numeros.length)];
+		numeros.splice(numeros.indexOf(n1), 1);
+		numerosEscolhidos.push(n1);
+	}
+
+	/* Enviando os resultados para o DOM */
+	for (let i = 0; i < 6; i++) {
+		let numero = numerosEscolhidos[i];
+		if (numero < 10) {
+			document.getElementById("n" + (i + 1)).innerHTML = "0" + numero;
+		} else {
+			document.getElementById("n" + (i + 1)).innerHTML = numero;
+		}
+	}
 }
 
 /* ================================================================= */
 
-/* Enviando os resultados para o DOM */
+/* ================================================================= */
+// funcionamento dos botões
 
-for (let i = 0; i < 6; i++) {
-  let numero = numerosEscolhidos[i];
-  if (numero < 10) {
-    document.getElementById("n" + (i + 1)).innerHTML = "0" + numero;
-  } else {
-    document.getElementById("n" + (i + 1)).innerHTML = numero;
-  }
+const refresh = document.getElementById("refresh");
+
+function refreshResult() {
+	randomNumbers();
 }
 
-/* ================================================================= */
+refresh.addEventListener("click", refreshResult);
 
-/* ================================================================= */
-// funcionamento dos botões do header
+const copyGame = document.getElementById("copyGame");
 
-btn1 = document.getElementById("btn1");
-btn1.onclick = function () {
-  tornarVisivel("comoFunciona");
-};
-
-btn2 = document.getElementById("btn2");
-btn2.onclick = function () {
-  tornarVisivel("contato");
-};
-
-function tornarVisivel(idElemento) {
-  var elemento = document.getElementById(idElemento);
-
-  if (elemento.style.display == "block") {
-    elemento.style.display = "none";
-  } else {
-    elemento.style.display = "block";
-  }
+function copyGameFunction() {
+	console.log("ok copy");
 }
+
+copyGame.addEventListener("click", copyGameFunction);
